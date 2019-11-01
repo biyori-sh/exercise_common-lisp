4aa1971b8b7626cd5ae6ee24d200e3557391d60b
biyori-sh <shimojihiromu@gmail.com>
2019-11-01 14:37:53 +0900

    modify

--- factorial.lisp
+++ factorial.lisp
@@ -1,4 +1,12 @@
-(defun fact (n)
+;; (declaim (ftype (function (fixnum) (values fixnum &optional)) fact))
+(defun fact-r (n)
   (declare (type fixnum n))
   (if (<= n 1) 1
-       (* n (fact (1- n)))))
+      (* n (fact-r (1- n)))))
+
+(defun fact-tro (n)
+  (declare (type fixnum n))
+  (labels ((sub-fact (n acc)
+	     (if (<= n 1) acc
+		 (sub-fact (1- n) (* n acc)))))
+    (sub-fact n 1)))
