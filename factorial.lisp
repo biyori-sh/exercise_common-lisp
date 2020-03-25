896690f5a392bc53066005423db29bb0b63a7702
biyori-sh <shimojihiromu@gmail.com>
2020-03-25 13:48:52 +0900

    modified

--- factorial.lisp
+++ factorial.lisp
@@ -1,23 +1,25 @@
-(declaim (ftype (function (fixnum) (values fixnum &optional)) fact))
 (defun fact (n)
-  (declare (type fixnum n))
+  "simple implementation"
   (if (<= n 1) 1
       (* n (fact (1- n)))))
 
-(defun fact-r (n)
+(declaim (ftype (function (fixnum) (values fixnum &optional)) fact-fn))
+(defun fact-fn (n)
+  "factorial calclation in type:fixnum"
   (declare (type fixnum n))
   (if (<= n 1) 1
-      (* n (fact-r (1- n)))))
+      (* n (fact-fn (1- n)))))
 
 (defun fact-tro (n)
-  (declare (type fixnum n))
+  "tail recusion"
   (labels ((sub-fact (n acc)
 	     (if (<= n 1) acc
 		 (sub-fact (1- n) (* n acc)))))
     (sub-fact n 1)))
 
-(defun fact-dotimes (n)
+(defun fact-do (n)
+  "not rucusive"
   (let ((result 1))
-    (dotimes (i n)
-      (setq result (* result (1+ i))))
-    result))
+    (do ((i 1 (1+ i)))
+	((> i n) result)
+      (setq result (* result (1+ i))))))
--- fibonacci.lisp
+++ fibonacci.lisp
@@ -1,5 +1,40 @@
 (defun fib (n)
+  "simple implementation"
   (cond ((<= n 0) 0)
 	((= n 1) 1)
 	(t (+ (fib (- n 1))
 	      (fib (- n 2))))))
+
+(declaim (inline fib-opt) (ftype (function (integer) integer) fib-opt))
+(defun fib-opt (n)
+  "practice to optimize"
+  (if (<= n 1) n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+(defun fib-tr (n)
+  "tail recusion"
+  (labels ((ftr-sub (cnter prev curr)
+	     (cond ((= cnter 0) prev)
+		   ((= cnter 1) curr)
+		   (t (ftr-sub (1- cnter) curr (+ prev curr))))))
+    (ftr-sub n 0 1)))
+
+(defun fib-matrix (n)
+  "fibonacci matrix"
+  (labels ((product-matrix (ma mb)
+	     (let ((result (make-array '(2 2) :initial-element 0)))
+	       (dotimes (i 2)
+		 (dotimes (j 2)
+		   (let ((res (aref result i j)))
+		     (dotimes (k 2)
+		       (setf res (+ res (* (aref ma i k) (aref mb k j)))))
+		     (setf (aref result i j) res))))
+	       result)))
+    (let ((fmatrix (make-array '(2 2) :initial-contents '((1 1) (1 0))))
+	  (identity (make-array '(2 2) :initial-contents '((1 0) (0 1)))))
+      (dotimes (i n)
+	(setf identity (product-matrix fmatrix identity)))
+      (values (aref identity 0 1) identity))))
+
+(defun binary-lst (n)
+  (mapcar #'digit-char-p (concatenate 'list (format nil "~b" n))))
