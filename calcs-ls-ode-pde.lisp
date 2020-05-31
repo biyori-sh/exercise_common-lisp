9ee509c084864ac5fdf0c2920519c8195c72da6e
biyori-sh <shimojihiromu@gmail.com>
2020-06-01 00:52:28 +0900

    added monte-carlo-integral

--- calcs-ls-ode-pde.lisp
+++ calcs-ls-ode-pde.lisp
@@ -1,4 +1,6 @@
-;; (setq *read-default-float-format* 'double-float)
+(setq *read-default-float-format* 'double-float)>
+(ql:quickload :lparallel)
+(setf lparallel:*kernel* (lparallel:make-kernel 2))
 
 ;;; line searches
 (defun golden-divide-method (init end func &optional (eps 1.0e-6))
@@ -136,21 +138,81 @@
 	    (+ curr-value (/ (+ k_1 (* 2 k_2) (* 2 k_3) k_4) 6.0)))))
 
 
-;; some exaples
-(defun exaple-euler-method ()
+;; Monte Carlo Integral
+(defun std-deviation (sample)
+  (let* ((sample-size (length sample))
+	 (mean (/ (reduce #'+ sample) sample-size)))
+    (sqrt (/ (reduce #'+ (mapcar (lambda (x) (expt (- x mean) 2)) sample))
+	     sample-size))))
+
+(defun monte-carlo-integral (func inits ends sample-size &key (constraint nil))
+  (let ((sample nil)
+	(intervals (mapcar #'- ends inits))
+	(volume 0.0e0)
+	(sample-point nil)
+	(cnt-accept 0)
+	(accept-rate 0)
+	(result 0.0e0))
+    (setf volume (float (reduce #'* intervals)))
+    (dotimes (i sample-size)
+      (setf sample-point
+	    (mapcar (lambda (x y) (+ x (* (random 1.0e0) y))) inits intervals))
+      (when (or (null constraint) (apply constraint sample-point))
+	(push (apply func sample-point) sample)
+	(incf cnt-accept)))
+    (setf accept-rate (/ cnt-accept sample-size)
+	  result (* (reduce #'+ sample) (/ volume sample-size)))
+    (let ((estmtd-vol (* accept-rate volume))
+	  (stdev 0.0e0))
+      (setf stdev (/ (std-deviation (mapcar (lambda (x) (* estmtd-vol x)) sample)) (sqrt cnt-accept)))
+      (values result stdev accept-rate estmtd-vol (/ estmtd-vol (sqrt sample-size))))))
+
+;; (defun monte-carlo-integral-map (func init end sample-size)
+;;   (let ((sample (make-array sample-size
+;; 			    :element-type *read-default-float-format*
+;; 			    :initial-element 1.0e0))
+;; 	(interval (- end init))
+;; 	(result 0.0e0)
+;; 	(abs-err 0.03e0))
+;;     (setf sample (map 'vector
+;; 		      (lambda (x) (+ init (* interval (random x))))
+;; 		      sample))
+;;     (setf sample (map 'vector func sample))
+;;     (setf result (/ (* (reduce #'+ sample) interval) sample-size))
+;;     (setf abs-err (/ result (sqrt sample-size)))
+;;     (values result abs-err)))
+
+;; some examples
+(defun example-euler-method ()
   (labels ((test-func ()
 	     (lambda (val-t val-x) (* 500 val-x val-x (- 1 val-x)))))
     (plot-first-order-ode 'euler-method (test-func) (float 1/100) (float 1/200) 200 200)
     (plot-first-order-ode 'euler-method (test-func) (float 1/100) (float 1/400) 400 400)))
 
-(defun exaple-implicit-euler-method ()
+(defun example-implicit-euler-method ()
   (labels ((test-func ()
 	     (lambda (val-t val-x) (* 500 val-x val-x (- 1 val-x)))))
     (plot-first-order-ode 'implicit-euler-method (test-func) (float 1/100) (float 1/200) 200 200)
     (plot-first-order-ode 'implicit-euler-method (test-func) (float 1/100) (float 1/400) 400 400)))
 
-(defun exaple-runge-kutta-method ()
+(defun example-runge-kutta-method ()
   (labels ((test-func ()
 	     (lambda (val-t val-x) (* 500 val-x val-x (- 1 val-x)))))
     (plot-first-order-ode 'runge-kutta-method (test-func) (float 1/100) (float 1/200) 200 200)
     (plot-first-order-ode 'runge-kutta-method (test-func) (float 1/100) (float 1/400) 400 400)))
+
+(defun example-monte-carlo-integral ()
+  (labels ((unit-3dim-ball ()
+	     (lambda (x y) (* 2 (sqrt (- 1.0e0 (* x x) (* y y))))))
+	   (in-unit-disk ()
+	     (lambda (x y) (< (+ (* x x) (* y y)) 1.0))))
+    (let ((value 0)
+	  (stdev 0)
+	  (acc-rate 0)
+	  (estimated-vol 0)
+	  (vol-stdev 0))
+      (setf (values value stdev acc-rate estimated-vol vol-stdev)
+	    (monte-carlo-integral (unit-3dim-ball) '(-1 -1) '(1 1) 1000000
+				  :constraint (in-unit-disk)))
+      (format t "Integrated value: (exact) 4*pi/3 = 4.18879..., (MC) ~6Fpm~,5F(1sigma)~%" value stdev)
+      (format t "Constrainted area: (exact) pi, (MC) ~6Fpm~,5F(1simga)~%" estimated-vol vol-stdev))))
