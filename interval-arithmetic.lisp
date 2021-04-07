fdd078346535fe220b920f3b69791633d359e25f
biyori-sh <shimojihiromu@gmail.com>
2021-04-07 19:39:38 +0900

    update *[]

--- interval-arithmetic.lisp
+++ interval-arithmetic.lisp
@@ -3,8 +3,10 @@
 ;; Marco Antoniotti
 ;; https://arxiv.org/pdf/2003.03831.pdf
 
+;; only fo SBCL
 (defparameter *default-rounding-mode*
-  (getf (sb-int:get-floating-point-modes) :rounding-mode))
+  (getf (sb-int:get-floating-point-modes) :ROUNDING-MODE)
+  "a keyword of the default rounding mode")
 
 ;; Macro for rounding mode
 (defmacro round-to (mode-keyword &body body)
@@ -35,6 +37,15 @@
   `(prog2 (sb-int:set-floating-point-modes :ROUNDING-MODE :NEAREST)
        (progn ,@body)
      (sb-int:set-floating-point-modes :ROUNDING-MODE *default-rounding-mode*)))
+
+
+;; type
+(deftype positive-real ()
+  '(real (0) *))
+
+(deftype negative-real ()
+  '(real * (0)))
+
 
 ;; Structure type of Interval
 ;; check large/small relation between low and high, required functions?
@@ -75,7 +86,7 @@
   (- ([]-high i) ([]-low i)))
 
 (defmethod radius ((i []))
-  (/ (diameteri i) 2.0))
+  (/ (diameter i) 2.0))
 
 (defmethod center ((i []))
   (/ (- ([]-high i) ([]-low i)) 2.0))
@@ -98,6 +109,7 @@
   (point->[] (abs i)))
 
 ;; Binary operators
+;; Addittion
 (defmethod +[] ((i1 []) (i2 []))
   ([] (round-negative (+ ([]-low i1) ([]-low i2)))
       (round-positive (+ ([]-high i1) ([]-high i2)))))
@@ -113,6 +125,7 @@
   ([] (round-negative (+ i1 i2))
       (round-positive (+ i1 i2))))
 
+;; Subtraction
 (defmethod -[] ((i1 []) (i2 []))
   (+[] i1 ([] (- ([]-low i2)) (- ([]-high i2)))))
 
@@ -124,6 +137,23 @@
 
 (defmethod -[] ((i1 real) (i2 real))
   (+[] (point->[] i1) (point->[] (- i2))))
+
+;; Multiplication
+;; (defmethod *[] ((i1 []) (i2 []))
+;;   ([] (round-negative
+;;         (cond ((and (positive[]p i1) (positive[]p i2))
+;;                (* ([]-low i1) ([]-low i2)))
+;;               ((and (negative[]p i1) (negative[]p i2))
+;;                (* ([]-high i1) ([]-high i2)))
+;;               (t (min (* ([]-low i1) ([]-high i2))
+;;                       (* ([]-high i1) ([]-low i2))))))
+;;       (round-positive
+;;         (cond ((and (positive[]p i1) (negative[]p i2))
+;;                (* ([]-low i1) ([]-high i2)))
+;;               ((and (negative[]p i1) (positive[]p i2))
+;;                (* ([]-high i1) ([]-low i2)))
+;;               (t (max (* ([]-high i1) ([]-high i2))
+;;                       (* ([]-low i1) ([]-low i2))))))))
 
 (defmethod *[] ((i1 []) (i2 []))
   ([] (round-negative
@@ -137,6 +167,22 @@
              (* ([]-high i1) ([]-low i2))
              (* ([]-low i1) ([]-low i2))))))
 
+(defmethod *[] ((i1 real) (i2 []))
+  ([] (round-negative
+        (min (* i1 ([]-high i2))
+             (* i1 ([]-low i2))))
+      (round-positive
+        (max (* i1 ([]-high i2))
+             (* i1 ([]-low i2))))))
+
+(defmethod *[] ((i1 []) (i2 real))
+  (*[] i2 i1))
+
+(defmethod *[] ((i1 real) (i2 real))
+  ([] (round-negative (* i1 i2))
+      (round-positive (* i1 i2))))
+
+;; Division
 (defmethod /[] ((i1 []) (i2 []))
   (if (zero[]p i2)
       (error "Not defined for the divisor including zero: ~a~%" i2)
@@ -153,11 +199,15 @@
 (defmethod /[] ((i1 real) (i2 real))
   (*[] (point->[] i1) (point->[] (/ 1.0 i2))))
 
-
-;; (let ((result ([] 0 0)))
+;; (let ((result ([] 0.0 0.0)))
 ;;   (dotimes (i 100 result)
 ;;     (setf result (+[] 0.1 result))))
 ;; => #S([] :LOW 9.999982 :HIGH 10.000027)
+
+;; (let ((result ([] 1.0 1.0)))
+;;         (dotimes (i 100 result)
+;;           (setf result (*[] ([] 0.95 0.95)  result))))
+;; => #S([] :LOW 0.0059204977 :HIGH 0.00592055)
 
 ;; (sb-int:get-floating-point-modes)
 ;; (sb-int:set-floating-point-modes :ROUNDING-MODE :POSITIVE-INFINITY)
